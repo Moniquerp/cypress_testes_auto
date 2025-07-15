2e55f7ca551f4685fa9104e7f26e6e420aa0ead1
Monique <moniquerpulcheri@gmail.com>
2025-07-14 21:47:09 -0300

    update cypress

--- cypress/e2e/Sauce_demo/login.cy.js
+++ cypress/e2e/Sauce_demo/login.cy.js
@@ -1,6 +1,9 @@
 describe('Suite de testes relacionada a sauce demo', () => {
     it('login com sucesso', () => { 
          cy.visit('https://www.saucedemo.com/')
+         cy.get('[data-test="username"]').type('problem_user')
+         cy.get('[data-test="password"]').type('secret_sauce')
+         cy.get('[data-test="login-button"]').click()
     })
 })
  
