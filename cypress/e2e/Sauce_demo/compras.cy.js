3333bfabd603c711fac7744bdb12d503a8e12f1e
Monique <moniquerpulcheri@gmail.com>
2025-07-24 20:14:53 -0300

    update

--- cypress/e2e/Sauce_demo/compras.cy.js
+++ cypress/e2e/Sauce_demo/compras.cy.js
@@ -2,28 +2,27 @@ describe('Testes relacionados a compras', () => {
 
     it('Realizando compra com sucesso', () => { 
         cy.visit('https://www.saucedemo.com/')
-        cy.get('.login_logo').should('be.visible').contains('Swag Labs') // validar que foi pra pag "swag labs"
+        cy.get('.login_logo').should('be.visible', {timeout: 20000}).and('have.text','Swag Labs') // validar que foi pra pag "swag labs"
         cy.get('[data-test="username"]').type('standard_user') // Realizar login com usuário "standart"
         cy.get('[data-test="password"]').type('secret_sauce') // senha
         cy.get('[data-test="login-button"]').click() // botao login
-        cy.get('[data-test="title"]').should('be.visible').contains('Products') //Validar que foi para a página "Products"
+        cy.get('[data-test="title"]').should('be.visible').and('have.text','Products') //Validar que foi para a página "Products"
         cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click() //Adicionar 4 itens
         cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click()
         cy.get('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click()
         cy.get('[data-test="add-to-cart-sauce-labs-fleece-jacket"]').click() //Adicionar 4 itens
         cy.get('[data-test="shopping-cart-link"]').click() //Ir para carrinho
-        cy.get('[data-test="title"]').should('be.visible').contains('Your Cart') //Validar que foi para a página "Your Cart"
+        cy.get('[data-test="title"]').should('be.visible').and('have.text','Your Cart') //Validar que foi para a página "Your Cart"
         cy.get('.cart_item').should('be.visible').and('have.length', 4) //entrar no carrinho, validar itens que foram add
         cy.get('[data-test="checkout"]').click() // ira para checkout
-        cy.get('[data-test="title"]').should('be.visible').contains('Checkout: Your Information') //Validar que foi para a página "Checkout Your Information"
+        cy.get('[data-test="title"]').should('be.visible').and('have.text','Checkout: Your Information') //Validar que foi para a página "Checkout Your Information"
         cy.get('[data-test="firstName"]').type('Monique') //Preencher campos de checkout Nome
         cy.get('[data-test="lastName"]').type('Rodrigues') //Preencher campos de checkout sobrenome
-        cy.get('[data-test="postalCode"]').type('29090490') //Preencher campos de checkout cep
+        cy.get('[data-test="postalCode"]').type('11111111') //Preencher campos de checkout cep
         cy.get('[data-test="continue"]').click() //Ir para overview
-        cy.get('[data-test="title"]').should('be.visible').contains('Checkout: Overview') //Validar que foi para a página "Checkout: Overview"                                
-        cy.get('[data-test="finish"]').click() //Ir para finish                              
-        cy.get('[data-test="title"]').should('be.visible').contains('Checkout: Complete!')//Validar que foi para a página "Checkout: Complete!"                                
-        cy.get('[data-test="complete-header"]').should('be.visible').contains('Thank you for your order!')//Validar mensagem de sucesso "Thank you for your order!"
-
-})
+        cy.get('[data-test="title"]').should('be.visible').and('have.text','Checkout: Overview') //Validar que foi para a página "Checkout: Overview"                                
+        cy.get('[data-test="finish"]').scrollIntoView({duration:1000}).click() //Ir para finish                              
+        cy.get('[data-test="title"]').should('be.visible').and('have.text','Checkout: Complete!')//Validar que foi para a página "Checkout: Complete!"                                
+        cy.get('[data-test="complete-header"]').should('be.visible').and('have.text','Thank you for your order!')//Validar mensagem de sucesso "Thank you for your order!"
+    })
 })
--- cypress/e2e/Sauce_demo/redes-sociais.cy.js
+++ cypress/e2e/Sauce_demo/redes-sociais.cy.js
@@ -5,20 +5,15 @@ describe('Teste relacionados a redes sociais', () => {
         cy.get('.login_logo').should('be.visible').contains('Swag Labs') // validar que foi pra pag "swag labs"
         cy.get('[data-test="username"]').type('visual_user') // Realizar login com usuário "visual_user"
         cy.get('[data-test="password"]').type('secret_sauce') // senha
-        cy.get('[data-test="login-button"]').click() // botao login
+        cy.get('[data-test="login-button"]').click({force:true}) // botao login
         cy.get('[data-test="title"]').should('be.visible').contains('Products') //Validar que foi para a página "Products"
-        cy.get('footer').scrollIntoView()// Utilizar a função .scrollIntoView() para ir até as redes sociais no rodapé da página
-        cy.get('ul.social').should('have.length', 1)// Validar que possui 3 redes sociais
-        cy.get('ul.social').should('have.text', 'TwitterFacebookLinkedIn')// Validar q contém nomes "Twitter" "Facebook"  "Linkedin" exatamente igual
-       
-        cy.get('[href="https://twitter.com/saucelabs"]') // Validar se cada rede social possui um link  
-
-        cy.get('[data-test="social-twitter"]').should('have.href','"https://twitter.com/saucelabs"')// Validar se twitter possui um link 
-        cy.get('[data-test="social-facebook"]').should('have.href','"https://www.facebook.com/saucelabs"')// Validar se facebook possui um link 
-        cy.get('[data-test="social-linkedin"]').should('have.href','"https://www.linkedin.com/company/sauce-labs/"')// Validar se linkedin possui um link 
-
-
+        cy.get('footer').scrollIntoView({duration:1000}) // Utilizar a função .scrollIntoView() para ir até as redes sociais no rodapé da página
+        cy.get('ul.social li').should('have.length', 3) // Validar que possui 3 redes sociais
+        cy.get('ul.social li.social_twitter').should('have.text', 'Twitter') // Validar q contém nome "Twitter" exatamente igual
+        cy.get('ul.social li.social_facebook').should('have.text', 'Facebook') // Validar q contém nome "Facebook" exatamente igual
+        cy.get('ul.social li.social_linkedin').should('have.text', 'LinkedIn') // Validar q contém nome "Linkedin" exatamente igual
+        cy.get('[data-test="social-twitter"]').should('have.attr','href','https://twitter.com/saucelabs') // Validar se twitter possui um link 
+        cy.get('[data-test="social-facebook"]').should('have.attr','href','https://www.facebook.com/saucelabs') // Validar se facebook possui um link 
+        cy.get('[data-test="social-linkedin"]').should('have.attr','href','https://www.linkedin.com/company/sauce-labs/') // Validar se linkedin possui um link 
     })
-
-
 })
