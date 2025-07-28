e5d617fdb0ad040bc8136e4c219bef4b3f4b79ad
Monique <moniquerpulcheri@gmail.com>
2025-07-28 19:50:15 -0300

    update

--- cypress/e2e/practice_expandtesting/validation_page_check.cy.js
+++ cypress/e2e/practice_expandtesting/validation_page_check.cy.js
@@ -1,14 +1,10 @@
 describe('Validação do check', () => {
    
     it('Validar se realizou screenshot da pagina toda', () => { 
-        cy.visit('https://practice.expandtesting.com/dynamic-pagination-table') //visitar site
-        cy.get('h1').should('be.visible').and('have.text','Dynamic pagination Table page for Automation Testing Practice') //Validar que foi para a página "Dynamic pagination Table.."
-        cy.get('select').select('10').should('have.value', '10') //utilizar o comando select() no elemento e selecionar a opcao 10
-        cy.get('td.sorting_1').should('be.visible').and('have.length', 10)//validar que existem 10 itens na tabela
-        
-        cy.get('.sorting_asc').should('have.text','Student Name')
-        cy.get('[aria-label="Gender: activate to sort column ascending"]').should('have.text','Gender')
-        cy.get('.form-control').type('Alice Johnson') //validar se tabela após consulta do nome, possui apenas 1 item
+        cy.visit('https://practice.expandtesting.com/checkboxes') //visitar site
+        cy.get('[page-title="text-capitalize h2 mb-4"]').should('have.text','Sample Checkboxes page for practice test automation') //Validar que foi para a página "Sample Checkboxes .."
+        cy.get('#checkbox1').check('checkbox1')
+        cy.get('#checkbox1').should('be.checked') //validar se tabela após consulta do nome, possui apenas 1 item
     })
 
 })
--- cypress/e2e/practice_expandtesting/validation_page_select.cy.js
+++ cypress/e2e/practice_expandtesting/validation_page_select.cy.js
@@ -1,29 +1,17 @@
 describe('Validação do select', () => {
    
-    it.only('Validar se realizou screenshot da pagina toda', () => { 
+    it('Validar se realizou screenshot da pagina toda', () => { 
         cy.visit('https://practice.expandtesting.com/dynamic-pagination-table') //visitar site
         cy.get('h1').should('be.visible').and('have.text','Dynamic pagination Table page for Automation Testing Practice') //Validar que foi para a página "Dynamic pagination Table.."
-        cy.get('select').select('10').should('have.value', '10') //utilizar o comando select() no elemento e selecionar a opcao 10
-        cy.get('td.sorting_1').should('be.visible').and('have.length', 10)//validar que existem 10 itens na tabela
-        
+        cy.get('label .form-select').select('10').should('have.value', '10') //utilizar o comando select() no elemento e selecionar a opcao 10
+        cy.get('#demo tr').should('be.visible').and('have.length', 10)//validar que existem 10 itens na tabela
         cy.get('.sorting_asc').should('have.text','Student Name')
-        //cy.get('.sorting').should('have.text','Student Name')
-        //cy.get('[aria-label="Student Name: activate to sort column ascending"]').should('have.text','Student Name')
-        cy.get('[aria-label="Gender: activate to sort column ascending"]').should('have.text','Gender')
-        cy.get('[aria-label="Class Level: activate to sort column ascending"]').should('have.text','Class Level')
-        cy.get('[aria-label="Home State: activate to sort column ascending"]').should('have.text','Home State')
-        cy.get('[aria-label="Major: activate to sort column ascending"]').should('have.text','Major')
-        cy.get('[aria-label="Extracurricular Activity: activate to sort column ascending"]').should('have.text','Extracurricular Activity')
-        //cy.get('[aria-label="Extracurricular Activity: activate to sort column descending"]').should('have.text','Extracurricular Activity')
-        
-        cy.get('.form-control').type('Alice Johnson')
-        //validar se a tabela após consulta do nome, possui apenas 1 item
-    })
-
-    it('Wait from page', () => { 
-        cy.visit('https://practice.expandtesting.com/')
-        cy.get('.gap-3 [href="/xpath-css-tester"]').click()
-        cy.wait(10000)
-        cy.get('h1').should('have.text', 'Xpath and Css Selector Tester Free Tools - Automation Testing Practice')
+        cy.get('[aria-label="Gender: activate to sort column ascending"]').should('be.visible').and('have.text','Gender')
+        cy.get('[aria-label="Class Level: activate to sort column ascending"]').should('be.visible').and('have.text','Class Level')
+        cy.get('[aria-label="Home State: activate to sort column ascending"]').should('be.visible').and('have.text','Home State')
+        cy.get('[aria-label="Major: activate to sort column ascending"]').should('be.visible').and('have.text','Major')
+        cy.get('[aria-label="Extracurricular Activity: activate to sort column ascending"]').should('be.visible').and('have.text','Extracurricular Activity')
+        cy.get('.form-control-sm').type('Alice Johnson') //validar se tabela após consulta do nome, possui apenas 1 item
+        cy.get('#demo tr').should('be.visible').and('have.length', 1)
     })
 })
