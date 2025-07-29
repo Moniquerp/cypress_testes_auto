f4a517f82ea04e0cf443e32b8a2fd115ccdeb08e
Monique <moniquerpulcheri@gmail.com>
2025-07-28 22:01:20 -0300

    update

--- cypress/e2e/practice_expandtesting/validation_page_check.cy.js
+++ cypress/e2e/practice_expandtesting/validation_page_check.cy.js
@@ -1,10 +1,20 @@
-describe('Validação do check', () => {
+describe('Validação do checkbox', () => {
    
-    it('Validar se realizou screenshot da pagina toda', () => { 
+    it('Validação do check', () => { 
         cy.visit('https://practice.expandtesting.com/checkboxes') //visitar site
-        cy.get('[page-title="text-capitalize h2 mb-4"]').should('have.text','Sample Checkboxes page for practice test automation') //Validar que foi para a página "Sample Checkboxes .."
-        cy.get('#checkbox1').check('checkbox1')
-        cy.get('#checkbox1').should('be.checked') //validar se tabela após consulta do nome, possui apenas 1 item
+        cy.get('.text-capitalize').should('have.text','Sample Checkboxes page for practice test automation') //Validar página "Sample Checkboxes .."
+        cy.wait(1000)
+        cy.get('#checkbox1').scrollIntoView({duration:1000}).check() //clicar no checkbox
+        cy.wait(1000)
+        cy.get('#checkbox1').should('be.checked')//Validar que realizou o check ("be checked")   
     })
-
+    
+    it('Validação do uncheck', () => { 
+        cy.visit('https://practice.expandtesting.com/checkboxes') //visitar site
+        cy.get('.text-capitalize').should('have.text','Sample Checkboxes page for practice test automation') //Validar página "Sample Checkboxes .."
+        cy.wait(1000)
+        cy.get('#checkbox2').scrollIntoView({duration:1000}).uncheck() //desmarcar o checkbox
+        cy.wait(1000)
+        cy.get('#checkbox2').should('not.to.be.checked')//validar que realizou o check ("be unchecked")
+    })
 })
