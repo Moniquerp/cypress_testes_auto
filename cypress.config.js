104f9bb16af2f9fd41cf59c470342e3e2db3f61d
Monique <moniquerpulcheri@gmail.com>
2025-08-06 12:22:39 -0300

    update

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   fixturesFolder: false,
+  viewportWidth: 1500,
+  viewportHeight: 900,
   e2e: {
     setupNodeEvents(on, config) {},
     env: {
--- cypress/e2e/practice_expandtesting/validation_radio_buttons.cy.js
+++ cypress/e2e/practice_expandtesting/validation_radio_buttons.cy.js
@@ -4,7 +4,7 @@ describe('Validação dos radio buttons', () => {
         cy.visit('https://practice.expandtesting.com/radio-buttons') //visitar site
         cy.get('h1').should('be.visible').and('have.text','Radio Buttons page for Automation Testing Practice') //Validar que foi para a página "Radio Buttons page.."
         cy.get('#yellow').scrollIntoView({duration:1000}).check() //utilizar o comando e selecionar opção yellow
-        //cy.get('row-md-6').should('have.length', 5)//validar que existem 5 itens na lista dos radio buttons referente a cor favorita
+        cy.get('input[name="color"]').should('have.length', 5)//validar que existem 5 itens na lista dos radio buttons referente a cor favorita
         cy.get('#yellow').should('be.checked') //validar se está selecionado o botão yellow
     })
 })
